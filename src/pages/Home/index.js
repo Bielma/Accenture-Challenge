d3fe7c7c62f4053c3d0f4b819ed64d4eab43469c
bielma <abielmalopez@gmail.com>
2023-02-12 13:36:10 -0600

    Home:: add header, add styles,

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,17 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
-  return <div>index</div>;
+  return (
+    <div>
+      <div className="header">
+        <h5>
+          <span>[</span> Making your Life Easier <span>]</span>{" "}
+        </h5>
+        <h1>Discovering the wold</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
