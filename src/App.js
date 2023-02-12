8469e74b9dfdb104203b7ac2d425f203a2b23524
bielma <abielmalopez@gmail.com>
2023-02-12 15:59:00 -0600

    App:: add route to show post detail

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import PostDetail from "./pages/PostDetail";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/post/:id" element={<PostDetail />} />
       </Routes>
     </div>
   );
